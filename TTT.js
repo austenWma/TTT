8d46bb9405e9dc82e1aa1ad7349321d150e78e7b
Austen Ma <austenma@Austens-MacBook-Pro.local>
2017-09-18 15:49:04 -0700

    Set up initial board, implementing prompt

---
+++ TTT.js
@@ -0,0 +1,11 @@
+let prompt = require('prompt')
+
+let newBoard = "1|2|3\n- - -\n4|5|6\n- - -\n7|8|9"
+
+const playTTT = (board) => {
+  console.log('Board ready...')
+  console.log(board)
+  
+}
+
+playTTT(newBoard)
